34e213523d1b6c9606f245744ecfe863d1f3e09e
Aakash Shankar <aakashshankar217@gmail.com>
2021-07-04 12:05:16 +0530

    Added trial from b1 for test 3

---
+++ Test2/trialfromb3.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+#define endl '\n'
+
+using namespace std;
+
+void test_case() {
+    cout << "test case ";
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    int t;
+    int a;
+    cin >> a;
+    vector<int> x(a);
+    cin >> t;
+    while(t) {
+        test_case();
+        cout << t << endl;
+        t--;
+    }
+    return 0;
+}
